616ccdedcb01aa80e482245561d3c8082a8886f8
Yerasyl Alimbek <erasylalimbek302@gmail.com>
2025-10-16 16:43:51 +0500

    Added some instances

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,17 @@
 package com.company;
 
 import com.company.IComponent.Coffee;
+import com.company.concreteComponent.AmericanoCoffee;
 import com.company.concreteComponent.EspressoCoffee;
 import com.company.concreteDecorator.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Espresso Coffee
         Coffee espresso = new EspressoCoffee();
         System.out.println(espresso.description() + " Cost: " + espresso.cost());
 
+        // Adding toppings
         espresso = new MilkDecorator(espresso);
         System.out.println(espresso.description() + " Cost: " + espresso.cost());
 
@@ -17,5 +20,17 @@ public class Main {
 
         espresso = new WhippedCreamDecorator(espresso);
         System.out.println(espresso.description() +  " Cost: " + espresso.cost());
+
+        System.out.println();
+
+        // Americano Coffee
+        Coffee americano = new AmericanoCoffee();
+        System.out.println(americano.description() + " Cost: " + americano.cost());
+
+        // Adding toppings
+        americano = new SugarDecorator(americano);
+        System.out.println(americano.description() +  " Cost: " + americano.cost());
+
+
     }
 }
